ef65cc0bcb3e440d4e582325e402032382f2630e
Brama  Hendra Mahendra <jdoe@example.com>
2024-07-06 18:23:35 +0700

    services

---
+++ services/userService.go
@@ -0,0 +1,31 @@
+package services
+
+import (
+	"apps/models"
+	"apps/repositories"
+)
+
+func GetAllUsers() ([]models.User, error) {
+	var users []models.User
+	err := repositories.GetAllUsers(&users)
+	return users, err
+}
+
+func CreateUser(user models.User) error {
+	return repositories.CreateUser(&user)
+}
+
+func GetUserByID(id string) (models.User, error) {
+	var user models.User
+	err := repositories.GetUserByID(&user, id)
+	return user, err
+}
+
+func UpdateUser(user models.User) error {
+	return repositories.UpdateUser(&user)
+}
+
+func DeleteUser(id string) error {
+	var user models.User
+	return repositories.DeleteUser(&user, id)
+}
